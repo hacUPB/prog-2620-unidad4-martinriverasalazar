12a8fc596e47966c6162fcc5bb9d1e21099b5bc6
Martín Rivera Salazar <martin.rivera@upb.edu.co>
2026-03-25 15:42:49 -0500

    Se añaden ejercicios de listas

---
+++ Listas/ejercicio4.py
@@ -0,0 +1,24 @@
+from random import randint
+
+meses= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+lista= []
+
+for i in range(12):
+    dato = randint(20, 80)
+    lista.append(dato)
+
+print (lista)
+
+mayor = max(lista)
+veces = lista.count(mayor)
+mes = lista.index(mayor)
+
+if veces > 1:
+    lista_meses= []
+    for i in range(len(lista)):
+        if lista[i] == mayor:
+            lista_meses.append(i)
+    print ("Ventas mayores en: ")
+    for mes in lista_meses:
+        print (f"Mayor venta en: {meses[mes]}")
+
---
+++ Listas/ejercicio5.py
@@ -0,0 +1,8 @@
+fuerzas_aero = ["Lift", "Drag", "Thrust"]
+
+print (fuerzas_aero)
+
+texto = input("Ingrese la fuerza aerodinámica que nos hace falta: ")
+fuerzas_aero.insert(1, texto)
+
+print (fuerzas_aero)
