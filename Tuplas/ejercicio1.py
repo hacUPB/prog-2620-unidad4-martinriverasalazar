21981b0490481a65a68b05e0d9ddf41f50129dc8
Martín Rivera Salazar <martin.rivera@upb.edu.co>
2026-04-08 14:35:36 -0500

    Se añade ejercicio 1

---
+++ Tuplas/ejercicio1.py
@@ -0,0 +1,14 @@
+# Tupla vacía
+coordenada = ()
+
+# Tupla con elementos
+coordenada = (33.9425, -118.4081)  # LAX (Aeropuerto de Los Ángeles)
+
+# Tupla con un solo elemento (requiere coma al final)
+rumbo = (270,)  # Sin la coma sería tratado como un entero entre paréntesis
+
+# Tupla sin paréntesis (empaquetado implícito)
+avion_info = "Boeing 787", "Dreamliner", 2009, 242
+
+#SON INMUTABLES
+
